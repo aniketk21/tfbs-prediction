154d36b787567e0d36a0892648f8a36fcb8a7db0
Aniket Kulkarni <kaniket21@gmail.com>
2017-06-15 13:31:18 +0530

    Removed chrStart chrEnd features

--- model_ffnn.py
+++ model_ffnn.py
@@ -4,36 +4,40 @@ import random
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import Adadelta, Adagrad
-
+from keras.optimizers import *
+from keras import regularizers
 from keras.models import load_model
-#model = load_model('model_myc_ffnn.h5')
+model = load_model('model_myc_ffnn.h5')
 
-f = open('gm12878_chr22_mod_npscore.dat')
-
+#f = open('gm12878_chr22_mod_npscore.dat')
+#f = open('gm_50pct_sorted.csv')
+'''f = open('h1.dat')
 l = f.readlines()
 
 len_l = len(l)
 
 for i in range(len_l):
     l[i] = l[i].split()
+    del l[i][0]
+    del l[i][0]
 
 for i in range(len_l):
     l[i][0] = float(l[i][0])
     l[i][1] = float(l[i][1])
     l[i][2] = float(l[i][2])
     l[i][3] = float(l[i][3])
-    l[i][4] = float(l[i][4])
-    l[i][5] = float(l[i][5])
+    #if l[i][3] == 'no\n':
+    #    l[i][3] = 0.0
+    #else:
+    #    l[i][3] = 1.0
 
 random.shuffle(l)
-
-batch_size = 32
+'''
+batch_size = 64
 num_classes = 2
-epochs = 10
+epochs = 5
 
-dataset_split = 0.8
-
+dataset_split = 0
 def create_training_and_test_set(data, split=0.6):
     '''
         split: float, percentage to split
@@ -47,49 +51,53 @@ def create_training_and_test_set(data, split=0.6):
     len_data = len(data)
     
     for i in range(int(split * len_data)):
-        x_train.append([[data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]])
+        #x_train.append([[data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]])
+        x_train.append([[data[i][0], data[i][1], data[i][2]]])
         y_train.append(data[i][-1])
     
     for i in range(int(split * len_data), len_data):
-        x_test.append([[data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]])
+        #x_test.append([[data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]])
+        x_test.append([[data[i][0], data[i][1], data[i][2]]])
         y_test.append(data[i][-1])
 
     return (x_train, y_train), (x_test, y_test)
 
-
+'''
 print('Loading data')
 (x_train, y_train), (x_test, y_test) = create_training_and_test_set(l, split=dataset_split)
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
+#x_train = np.array(x_train)
+#y_train = np.array(y_train)
 x_test = np.array(x_test)
 y_test = np.array(y_test)
 
-x_train = x_train.reshape(int(dataset_split*len_l), 5)
-x_test = x_test.reshape(len_l - int(dataset_split*len_l), 5)
-x_train = x_train.astype('float32')
+#x_train = x_train.reshape(int(dataset_split*len_l), 3)
+x_test = x_test.reshape(len_l - int(dataset_split*len_l), 3)
+#x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
 
 #y_train = keras.utils.to_categorical(y_train, num_classes)
-#y_test = keras.utils.to_categorical(y_test, num_classes)
-
+y_test = keras.utils.to_categorical(y_test, num_classes)
+'''
 #### Train the model ####
 
-#'''
+'''
 model = Sequential()
-model.add(Dense(32, activation='relu', input_shape = (5, )))
-model.add(Dropout(0.2))
-model.add(Dense(12, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation='relu'))
+model.add(Dense(32, activation='relu', input_dim=3, kernel_regularizer=regularizers.l2(0.01)))#input_shape = (5, )))
+model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
 
 model.summary()
 
-model.compile(loss='mean_squared_error',
-              optimizer=Adagrad(),
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(),
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
@@ -100,8 +108,8 @@ history = model.fit(x_train, y_train,
 
 model.save('model_myc_ffnn.h5')
 f.close()
-#'''
 exit()
+'''
 #### Training ends ####
 #### Test the model ####
 
@@ -109,21 +117,26 @@ tp = 0 # True Pos
 fp = 0 # False Pos
 tn = 0 # True Neg
 fn = 0 # False Neg
-
-p = []
+'''
+p = ''
 cnt_pos = 0
 cnt_neg = 0
 for i in range(len(x_test)):
-    print(i)
+    print(float(i)/len_l, '%')
     x_test[i] = np.array(x_test[i])
-    x_test[i] = x_test[i].reshape(1, 5)
+    x_test[i] = x_test[i].reshape(1, 3)
     x_test[i] = x_test[i].astype('float32')
     
     predicted = model.predict(x_test[i], verbose=0)
-    predicted = int(predicted[0])
     
-    actual = int(y_test[i])
+    actual = y_test[i]
+    p += str([i, x_test[i], actual, predicted]) + '\n'
 
+w = open('op.dat', 'w')
+w.write(p)
+w.close()
+'''
+'''
     if (actual == 1) and (predicted == 1): # True Positive
         tp += 1
     if (actual == 0) and (predicted == 1): # False Positive
@@ -132,7 +145,7 @@ for i in range(len(x_test)):
         fn += 1
     if (actual == 0) and (predicted == 0): # True Negative
         tn += 1
-    
+'''
     
     #if (np.array_equal(np.array([0, 1]).reshape(1, 2).astype('float32'), predicted)):
     #    cnt_pos += 1
@@ -141,7 +154,8 @@ for i in range(len(x_test)):
         
     #if np.array_equal(output_cases['01'], op):
     #    print np.array_equal(actual, predicted)
-
+'''
+print('TP:', tp, ' ', 'TN:', tn, ' ', 'FP:', fp, 'FN:', fn)
 precision = recall = -1
 try:
     precision = float(tp) / (tp + fp)
@@ -155,9 +169,9 @@ except ZeroDivisionError as z:
 
 print('Precision:', precision)
 print('Recall:', recall)
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+'''
+#score = model.evaluate(x_test, y_test, verbose=0)
+#print('Test loss:', score[0])
+#print('Test accuracy:', score[1])
 
 f.close()
