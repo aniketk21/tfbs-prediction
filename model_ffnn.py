d49d1d0fd3ad63085811a7d65174fd13f677a68c
Aniket Kulkarni <kaniket21@gmail.com>
2017-06-20 17:46:51 +0530

    model_ffnn.py: add feed forward neural net model

--- model_ffnn.py
+++ model_ffnn.py
@@ -8,40 +8,43 @@ from keras.layers import Dense, Dropout
 from keras.optimizers import *
 from keras import regularizers
 from keras.models import load_model
-
-model = load_model('model_myc_ffnn.h5')
+import keras.backend as K
+#model = load_model('model_myc_ffnn_reg.h5')
 #model.summary()
 #f = open('gm12878_chr22_mod_npscore.dat')
 #f = open('gm_50pct_sorted.csv')
-print 'starting...'
-f = open('h1_chr15.dat')
+print 'opening file'
+f = open('peak_68pct.csv')
 l = f.readlines()
+random.shuffle(l)
+#random.shuffle(l)
+#random.shuffle(l)
 f.close()
+print 'starting'
+#random.shuffle(l)
 len_l = len(l)
 index = []
 for i in range(len_l):
-    l[i] = l[i].split()
-    index.append([l[i][0], l[i][1]])
-    del l[i][0]
-    del l[i][0]
+    l[i] = l[i].split(',')
 
 for i in range(len_l):
     l[i][0] = float(l[i][0])
     l[i][1] = float(l[i][1])
     l[i][2] = float(l[i][2])
     #l[i][3] = float(l[i][3])
-    #if l[i][3] == 'no\n':
-    #    l[i][3] = 0.0
-    #else:
-    #    l[i][3] = 1.0
+    if l[i][3] == 'no\n':
+        l[i][3] = 0.0
+    else:
+        l[i][3] = 1.0
 
-random.shuffle(l)
-
+#random.shuffle(l)
+def mean_pred(y_true, y_pred):
+    return K.mean(y_pred)
 batch_size = 64
 num_classes = 2
-epochs = 20
+epochs = 8
 
-dataset_split = 0
+dataset_split = 0.7
 
 def create_training_and_test_set(data, split=0.6):
     '''
@@ -52,18 +55,17 @@ def create_training_and_test_set(data, split=0.6):
     y_train = []
     x_test = []
     y_test = []
-    
     len_data = len(data)
     
     for i in range(int(split * len_data)):
         #x_train.append([[data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]])
         x_train.append([[data[i][0], data[i][1], data[i][2]]])
-        y_train.append(data[i][-1])
+        y_train.append(data[i][3])
     
     for i in range(int(split * len_data), len_data):
         #x_test.append([[data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]])
         x_test.append([[data[i][0], data[i][1], data[i][2]]])
-        y_test.append(data[i][-1])
+        y_test.append(data[i][3])
 
     return (x_train, y_train), (x_test, y_test)
 
@@ -73,24 +75,24 @@ print('Loading data')
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
-#x_train = np.array(x_train)
-#y_train = np.array(y_train)
+x_train = np.array(x_train)
+y_train = np.array(y_train)
 x_test = np.array(x_test)
-y_test = np.array(y_test)
+y_test = np.array(y_test).astype('float32')
 
-#x_train = x_train.reshape(int(dataset_split*len_l), 3)
+x_train = x_train.reshape(int(dataset_split*len_l), 3)
 x_test = x_test.reshape(len_l - int(dataset_split*len_l), 3)
-#x_train = x_train.astype('float32')
+x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
 
-#y_train = keras.utils.to_categorical(y_train, num_classes)
+y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 '''
 #### Train the model ####
 
 '''
-'''
+#'''
 model = Sequential()
 model.add(Dense(32, activation='relu', input_dim=3, kernel_regularizer=regularizers.l2(0.01)))#input_shape = (5, )))
 model.add(Dropout(0.5))
@@ -112,10 +114,9 @@ history = model.fit(x_train, y_train,
                     verbose=1,
                     validation_data=(x_test, y_test))
 
-model.save('model_myc_ffnn.h5')
-f.close()
-#exit()
-'''
+model.save('model_myc_ffnn_reg.h5')
+exit()
+#'''
 #### Training ends ####
 #### Test the model ####
 '''
@@ -125,19 +126,25 @@ tn = 0 # True Neg
 fn = 0 # False Neg
 
 p = ''
-cnt_pos = 0
-cnt_neg = 0
+'''
+
+cnt = 0
+acc = 0
+res = 0
+print('actual, predicted')
 for i in range(len(x_test)):
-    print(float(i)/(len_l - dataset_split*len_l), '%')
+    #print(float(i)/(len_l - dataset_split*len_l), '%')
     x_test[i] = np.array(x_test[i])
     x_test[i] = x_test[i].reshape(1, 3)
     x_test[i] = x_test[i].astype('float32')
     
     predicted = model.predict(x_test[i], verbose=0)
-    #predicted = int(predicted.item(0))
+    
     actual = y_test[i]
-    print(actual, predicted)
-'''
+
+    print(actual, predicted[0][0])
+
+#'''
 '''
     if (actual == 1) and (predicted == 1): # True Positive
         tp += 1
@@ -183,9 +190,9 @@ except ZeroDivisionError as z:
 print('Precision:', precision)
 print('Recall:', recall)
 '''
-#'''
+'''
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-#'''
+'''
 sys.stdout.flush()
