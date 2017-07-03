8a77730ea2bb1012a47136d428071e1adbafac30
Aniket Kulkarni <kaniket21@gmail.com>
2017-07-03 18:50:03 +0530

    dat2csv.py: add script to convert dat file to csv file

---
+++ dat2csv.py
@@ -0,0 +1,31 @@
+'''
+    dat2csv.py
+    usage: python dat2csv.py dataset.dat
+    convert the dat file to csv file so that it can fit in memory when opened in Weka
+'''
+import sys
+
+f = open(sys.argv[1])
+w = open(sys.argv[1]+'.csv', 'w')
+
+l = f.readlines()
+
+for i in range(len(l)):
+    l[i] = l[i].split()
+    del l[i][0]
+    del l[i][0]
+
+res = ''
+
+for i  in range(len(l)):
+    res += l[i][0]+','+l[i][1]+','+l[i][2]+','
+    if l[i][3] == '0':
+        res += 'no\n'
+    else:
+        res += 'yes\n'
+
+w.write('chrom,motif_score,motif,peak\n')
+w.write(res)
+
+w.close()
+f.close()
