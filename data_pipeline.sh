ab6cfe99c0f4f6aa6c3f326ea6266e053606ee72
Aniket Kulkarni <kaniket21@gmail.com>
2017-07-04 19:11:37 +0530

    data_pipeline.sh: add dnase command

--- data_pipeline.sh
+++ data_pipeline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # script to create a dataset that is compatible with the model's input
 
-python create_bp_windows.py gm12878_chr17.dat 0 81195107 81195200
-python mod_chrom.py gm12878_chr17.bed gm12878_chr17.dat gm12878_chr17_mod_chrom.dat
-python mod_motif.py gm12878_chr17_mod_chrom.dat chr17.gff gm12878_chr17_mod_motif.dat
-python mod_peak.py gm12878_chr17.narrowPeak gm12878_chr17_mod_motif.dat gm12878_chr17_mod_peak.dat
+python create_bp_windows.py k_chr8.dat 600 146363400 146363600 
+python mod_chrom.py k_chr8.bed k_chr8.dat k_chr8_mod_chrom.dat
+python mod_dnase_score.py k_chr8.bedgraph k_chr8_mod_chrom.dat k_chr8_mod_dnase.dat
+python mod_motif.py k_chr8_mod_dnase.dat chr8.gff k_chr8_mod_motif.dat
+python mod_peak.py k_chr8.tsv k_chr8_mod_motif.dat k_chr8_mod_peak_no_dnase.dat
