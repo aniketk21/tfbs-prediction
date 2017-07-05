d92f7cedc70faa0429db9b9b98072cffa4cc0d9f
Aniket Kulkarni <kaniket21@gmail.com>
2017-07-05 19:42:37 +0530

    data_pipeline.sh: change order of execution

--- data_pipeline.sh
+++ data_pipeline.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # script to create a dataset that is compatible with the model's input
 
-python create_bp_windows.py k_chr8.dat 600 146363400 146363600 
-python mod_chrom.py k_chr8.bed k_chr8.dat k_chr8_mod_chrom.dat
-python mod_dnase_score.py k_chr8.bedgraph k_chr8_mod_chrom.dat k_chr8_mod_dnase.dat
-python mod_motif.py k_chr8_mod_dnase.dat chr8.gff k_chr8_mod_motif.dat
-python mod_peak.py k_chr8.tsv k_chr8_mod_motif.dat k_chr8_mod_peak_no_dnase.dat
+python create_bp_windows.py gm_chr22.dat 16050000 51234702 51244566 
+python mod_chrom.py gm_chr22.bed gm_chr22.dat gm_chr22_mod_chrom.dat
+python mod_motif.py gm_chr22_mod_chrom.dat gm_chr22.gff gm_chr22_mod_motif.dat
+python mod_dnase_score.py gm_chr22.bedgraph gm_chr22_mod_motif.dat gm_chr22_mod_dnase.dat
+python mod_peak.py gm_chr22_e2f1.tsv gm_chr22_mod_dnase.dat gm_chr22_mod_peak.dat
+python dat2csv.py gm_chr22_mod_peak.dat
+python uniq.py gm_chr22_mod_peak.dat.csv
